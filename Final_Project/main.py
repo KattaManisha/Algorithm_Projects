6da4d5b65206736c988d797e5549f5aad52c75d2
“KattaManisha” <manishak@pdx.edu>
2024-03-10 21:02:25 -0700

    Change the variable names for readability and add uscities.csv
    Signed-off-by: “KattaManisha” <manishak@pdx.edu>

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -7,27 +7,24 @@ num_cities_list = [5, 10, 15]  # Varying number of cities for experimentation
 
 for num_cities in num_cities_list:
     print(f"Experimenting with {num_cities} cities:")
+    generate_graph = GenerateGraph(num_cities)
+    generate_graph.generate_random_graph()
+    generate_graph.assign_random_weights()
     
-    # Generate the graph
-    graph_gen = GenerateGraph(num_cities)
-    graph_gen.generate_random_graph()
-    graph_gen.assign_random_weights()
+    tsp_algorithms = TSPAlgorithms(num_cities)
+    tsp_algorithms.graph = generate_graph.graph
+    tsp_algorithms.coordinates = generate_graph.coordinates
     
-    # Solve with nearest neighbor algorithm
-    tsp_algo = TSPAlgorithms(num_cities)
-    tsp_algo.graph = graph_gen.graph
-    nn_tour = tsp_algo.nearest_neighbor_tour()
-    nn_tour_length = tsp_algo.calculate_tour_length(nn_tour)
+    nn_tour = tsp_algorithms.nearest_neighbor_tour()
+    nn_tour_length = tsp_algorithms.calculate_tour_length(nn_tour)
     
     print("Nearest Neighbor Tour:", nn_tour)
     print("Nearest Neighbor Tour Length:", nn_tour_length)
     
-    # Solve with Christofides algorithm
-    christofides_tour = tsp_algo.christofides_tour()
-    christofides_tour_length = tsp_algo.calculate_tour_length(christofides_tour)
+    christofides_tour = tsp_algorithms.christofides_tour()
+    christofides_tour_length = tsp_algorithms.calculate_tour_length(christofides_tour)
     print("Christofides Tour:", christofides_tour)
     print("Christofides Tour Length:", christofides_tour_length)
     
- # Plot the graph with tours
-    plot_graph = PlotGraph(graph_gen.graph, graph_gen.coordinates)
-    plot_graph.plot_with_tours(nn_tour, christofides_tour, nn_tour_length, christofides_tour_length, num_cities)
+    plot_graph = PlotGraph(generate_graph.graph, generate_graph.coordinates)
+    plot_graph.plot_with_tours([nn_tour, christofides_tour], ['r', 'b'], ['Nearest Neighbor Tour', 'Christofides Tour'], num_cities)
